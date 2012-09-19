50e6591763f11da695c79a816628d978b05ad313
bmael <maelbar44@gmail.com>
2012-09-19 20:12:08 +0200

    Documentation updated

--- Doc/html/search/all_61.js
+++ Doc/html/search/all_61.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['add',['add',['../class_linked_list.html#a2516f29b7b464ffd4615fcdab995015e',1,'LinkedList']]],
-  ['array',['Array',['../class_array.html',1,'']]]
+  ['add',['add',['../class_array.html#a1911ffe5822c9628a4a8035c93cf8c0d',1,'Array::add()'],['../class_linked_list.html#a2516f29b7b464ffd4615fcdab995015e',1,'LinkedList::add()']]],
+  ['array',['Array',['../class_array.html',1,'Array'],['../class_array.html#ae20b3dbb4aa6083679c0dea835abd861',1,'Array::Array()']]]
 ];
