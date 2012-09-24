98d80c16ed86199beb26d5671c80c5e2e7aa5ffd
bmael <maelbar44@gmail.com>
2012-09-24 17:43:03 +0200

    autoconf new options

---
+++ src/config.h
@@ -0,0 +1,23 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `pow' function. */
+/* #undef HAVE_POW */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "maelbar44@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Polynome"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Polynome 2.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "polynome"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.0"
